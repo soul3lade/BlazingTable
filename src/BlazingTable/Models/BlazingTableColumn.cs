49c7b67289184039455fc6c89bd5023e7cd71046
kxs763 <kyle.seman@sherwin.com>
2019-10-31 15:41:34 -0400

    Added settings model to help define different options for the table design Redesigned table layers to take settings into affect

---
+++ src/BlazingTable/Models/BlazingTableColumn.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazingTable.Models
+{
+    public class BlazingTableColumn
+    {
+        /// <summary>
+        /// Name of property to associate with
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Value to display in the table head
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Order to display column in
+        /// </summary>
+        public int Order { get; set; }
+
+        /// <summary>
+        /// Allows sorting by the column
+        /// </summary>
+        public bool Sortable { get; set; }
+
+        /// <summary>
+        /// Adds the column into the filter menu
+        /// </summary>
+        public bool Filterable { get; set; }
+
+        /// <summary>
+        /// Allows the user to show/hide the column
+        /// </summary>
+        public bool AllowHide { get; set; }
+
+        /// <summary>
+        /// Defines whether the column is currently hidden
+        /// </summary>
+        public bool Hidden { get; set; }
+
+        /// <summary>
+        /// Allows the column to be dynamically resized
+        /// </summary>
+        public bool AllowResize { get; set; }
+
+        public BlazingTableColumn()
+        {
+            Sortable = true;
+            Filterable = true;
+            Hidden = false;
+            AllowHide = true;
+            AllowResize = true;
+        }
+    }
+}
---
+++ src/BlazingTable/Models/BlazingTableSettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazingTable.Models
+{
+    public class BlazingTableSettings<T>
+    {
+        public string Title { get; set; }
+
+        public List<T> Data { get; set; }
+
+        /// <summary>
+        /// Defines custom settings for columns.  If null, defaults will be used instead
+        /// </summary>
+        public List<BlazingTableColumn> Columns { get; set; }
+
+        public string Width { get; set; }
+
+        public string Height { get; set; }
+
+
+    }
+}
